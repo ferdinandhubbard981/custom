cbe6fdeccf6d32087283456c3ffbf5c3027392fa
ferdinandhubbard981 <ferdinandhubbar@gmail.com>
2023-08-20 11:26:44 +0100

    first commit

---
+++ configs/lspconfig.lua
@@ -0,0 +1,33 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+local servers = { "html", "cssls", "tsserver", "clangd" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- Custom configuration for pylsp
+lspconfig.pylsp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    pylsp = {
+      configurationSources = { "pycodestyle" }, -- pycodestyle will read from setup.cfg
+      plugins = {
+        pycodestyle = {
+          enabled = true,
+          ignore = {'E501'},  -- Empty list to ignore nothing, will pull from setup.cfg
+        },
+      },
+    },
+  },
+}
+
+-- lspconfig.pyright.setup { blabla}
+
