65bc664459a55042f17f66166daaf72f9d853207
ferdinandhubbard981 <ferdinandhubbar@gmail.com>
2023-08-28 14:00:40 +0100

    added mapping for copying selected text to reverse ssh nc

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>c"] = { "<cmd> '<,'>:w !nc -q 0 localhost 2000<CR>", "Copy whole file" },
   },
 }
 
