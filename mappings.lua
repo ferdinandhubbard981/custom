ea7ac974998ae8e66a1bb6a415ab8c0963052654
ferdinandhubbard981 <ferdinandhubbar@gmail.com>
2023-08-28 14:13:08 +0100

    working ssh-copy

--- mappings.lua
+++ mappings.lua
@@ -4,7 +4,10 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>c"] = { "<cmd> '<,'>:w !nc -q 0 localhost 2000<CR>", "Copy whole file" },
+  },
+  v = {
+    ["z"] = { "<cmd> '<,'>:w !nc -q 0 localhost 2000<CR>", "Copy whole file" },
+
   },
 }
 
