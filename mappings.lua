51f79804c204b4859c2e61a4cc48fbd990ae08b5
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2024-09-18 21:28:32 +0100

    added keybinding

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,12 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>m"] = {
+      function()
+        vim.lsp.buf.format()
+      end,
+      "lsp format",
+    },
   },
 }
 
